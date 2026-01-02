4718db738b9b1176d4c4e401092247042dee29bf
ERDEM ERBABA <erdemerbaba@gmail.com>
2026-01-02 16:30:21 +0300

    Add Book model to define books table structure

--- app/models.py
+++ app/models.py
@@ -1,1 +1,14 @@
-will be added soon
+from flask_sqlalchemy import SQLAlchemy
+
+db= SQLAlchemy()
+
+class Book(db.Model):
+    """Defines the Book model mappe to the books table in postgresql."""
+    __tablename__ = 'books'
+    id = db.column(db.Integer, primary_key=True)
+    title = db.column(db.String(255),nullable=False)
+    author = db.column(db.String(255), nullable=False)
+    year = db.column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
